796b627c18693d9cc2ee621a87674e2491ad20f0
Arman Patel <vanixgaming634@gmail.com>
2023-04-29 21:17:33 +0530

    Info about some of the most used Globals in the NodeJs

---
+++ 01_Globals.js
@@ -0,0 +1,15 @@
+// Global variables in Node 
+
+//1] __dirname :- Path of the current directory
+  console.log("Path of the current directory: "+__dirname+"\n");  
+//D:\FullStack Webdev\NodeJs_ExpressJs\ExpressJs_Tutorials\Codes
+
+//2] __filename :- File name
+  console.log("File name: "+__filename+"\n");
+// D:\FullStack Webdev\NodeJs_ExpressJs\ExpressJs_Tutorials\Codes\01_Globals.js
+
+//3] module :- Info about current module
+
+//4] require :- Function to use modules
+
+//5] process :- Info about the environment where program is being executed
